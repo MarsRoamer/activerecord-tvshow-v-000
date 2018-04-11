bdadb01c7324aca79140ac05acdbb8b592b494dd
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-11 20:00:50 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by maximum(:rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.order(:rating).first
+  end
+
+  def self.ratings_sum
+
+  end
+
+
+
+end
